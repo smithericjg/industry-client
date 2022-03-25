bed7c986d8bf7d735986dafc045e0c15cdfe57e9
Michael Wood <michaelwood33311@icloud.com>
2022-03-25 15:19:10 -0400

    MVP 002

--- src/components/GetInTouch/GetInTouch.jsx
+++ src/components/GetInTouch/GetInTouch.jsx
@@ -12,20 +12,20 @@ export default class GetInTouch extends React.Component {
                 <div className="connect">
                     <img alt="Close X" className='closeX' src={close} onClick={this.props.handleClose}/>
                     <div className="connect__top">
-                        <div className="connect__top--header">Send <span>Spa Essentials</span> a message <img className='globeIcon' src={globe} alt="Globe Icon" /></div>
+                        <div className="connect__top--header">Send <span>{this.props.vendorName}</span> a message</div>
                         <div className="profile">
                             <div className="profile__avatar"><img src={Spa} alt="Spa" className="profile__avatar--img" /></div>
                             <div className="profile__text">
-                                <h4 className="profile__text--title">Spa Essentials{this.props.vendorName}</h4>
-                                <h5 className="profile__text--description">{this.props.information}The place to purchase all your spa essential needs. Ranging from equipment to fragrance oils, we offer</h5>
+                                <h4 className="profile__text--title">{this.props.vendorName}<img className='globeIcon' src={globe} alt="Globe Icon" /></h4>
+                                <h5 className="profile__text--description">{this.props.information}</h5>
                             </div>
                         </div>
                     </div>
                     <div className="divider"></div>
                     <div className="connect__bottom">
-                        <div className="connect__bottom--header">Let Spa Essentials know who you are</div>
+                        <div className="connect__bottom--header">Let {this.props.vendorName} know who you are</div>
                         <form action="submit">
-                            <input className='productInput' type="text" name="productInput" placeholder='Send quotes, description or details about your product'></input>
+                            <textarea className='productInput' name="productInput" placeholder='Send quotes, description or details about your product' />
                         </form>
                         <button className='productSubmit' alt="Send Message Button" onClick={this.props.handleConfirm}><h5 className='btn-text'>Send Message</h5></button>
                     </div>
